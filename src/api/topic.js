5b0160c843065d3704e6a25b76793b78048c33d7
kindlin2012 <77873944+kindlin2012@users.noreply.github.com>
2023-07-27 03:54:19 +0800

    list refresh mixin

---
+++ src/api/topic.js
@@ -0,0 +1,22 @@
+import { request } from '@/utils/request'
+
+export function getTopics(data) {
+  return request('topics', {
+    data: data
+  })
+}
+export function getCategories(data) {
+  return request('categories')
+}
+
+export function getTopic(id, data) {
+  return request('topics/' + id, {
+    data: data
+  })
+}
+
+export function getUserTopics(userId, data) {
+  return request('users/'+userId+'/topics', {
+    data: data
+  })
+}
