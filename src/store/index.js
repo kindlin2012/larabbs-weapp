be862d33ade3c1d807ed5ed729b2f212257996b1
kindlin2012 <77873944+kindlin2012@users.noreply.github.com>
2023-07-29 12:08:48 +0800

    unread notification

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 import Vuex from '@wepy/x';
 import users from './modules/user'
+import notification from './modules/notification'
+// 导出 store 对象
 
 export default new Vuex.Store({
   modules: {
     users,
+    notification,
   }
 })
